7d7947db85d7c7d79e168992899719cb8f8c8c64
Kal Zheng <kal.zheng@inspire-dt.com>
2023-12-29 16:14:49 +0800

    add remove places function

--- src/http.js
+++ src/http.js
@@ -3,7 +3,8 @@ export async function fetchAvailablePlaces() {
   const resData = await response.json();
 
   if (!response.ok) {
-    throw new Error("Failed to fetch places");
+    console.log('here');
+    throw new Error("Failed to fetch places....");
   }
   return resData.places;
 }
