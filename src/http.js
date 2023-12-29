4318fea8b225c89c7b5b48866c38470a6f664625
Kal Zheng <kal.zheng@inspire-dt.com>
2023-12-29 16:33:48 +0800

    add fetch user place function and display it on screen

--- src/http.js
+++ src/http.js
@@ -3,8 +3,17 @@ export async function fetchAvailablePlaces() {
   const resData = await response.json();
 
   if (!response.ok) {
-    console.log('here');
     throw new Error("Failed to fetch places....");
+  }
+  return resData.places;
+}
+
+export async function fetchUserPlaces() {
+  const response = await fetch("http://localhost:3000/user-places");
+  const resData = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places....");
   }
   return resData.places;
 }
